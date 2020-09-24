9e90abe40cda59f8e1cdc416c0e10f1a49eb9cba
Daffa Yumna <daffa.10rpl.2019@gmail.com>
2020-09-24 14:42:03 +0700

    DetailMovie

---
+++ app/src/main/java/com/daffa/myapplication/ListDataFavourite.java
@@ -0,0 +1,13 @@
+package com.daffa.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListDataFavourite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/daffa/myapplication/RealmHelper.java
@@ -0,0 +1,13 @@
+package com.daffa.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RealmHelper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
